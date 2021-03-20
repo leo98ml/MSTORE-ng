a40dd072f05230e8205b1984151bd1b59110c698
Leonardo Marafioti <leo98ml@gmail.com>
2021-03-20 20:16:54 +0100

    about us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from "./about-us/about-us.component"
+import { ItemContainerComponent } from "./item-container/item-container.component"
 
-
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "about", component: AboutUsComponent
+},
+{
+  path: "", component: ItemContainerComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
